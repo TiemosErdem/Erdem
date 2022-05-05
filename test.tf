c251a73a086af81116032fa01a7c90551674b40d
Buster <56392791+busterthethumper@users.noreply.github.com>
2022-05-05 18:22:34 +0100

    TEST UPDATE

--- test.tf
+++ test.tf
@@ -6,3 +6,11 @@ wordl is turning wowgreat wortlddsadasds resource "aws_route53_record" "best_web
   ttl     = "3600"
   records = ["LOADBALANCER_GOES_HERE"]
 }
+
+ resource "aws_route53_record" "testing_best_site_cname" {
+  zone_id = aws_route53_zone.hackney_gov_uk.zone_id
+  name    = "testing-best-site.hackney.gov.uk"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["LOADBALANCER_GOES_HERE"]
+}
