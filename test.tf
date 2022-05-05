97fe0a22e67d3ed2ccbd202b0c64b0062913810f
Buster <56392791+busterthethumper@users.noreply.github.com>
2022-05-05 19:08:21 +0100

    TEST UPDATE

--- test.tf
+++ test.tf
@@ -34,3 +34,10 @@ By busterthethumper [TEST TERRAFORM FILE - INFORMATION IN THIS FILE IS UPDATED M
   ttl     = "3600"
   records = ["LOADBALANCER_GOES_HERE"]
 }
+ resource "aws_route53_record" "flying_turtles_com_cname" {
+  zone_id = ZONE_ID_GOES_HERE
+  name    = "flyingturtles.com"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["LOADBALANCER_GOES_HERE"]
+}
