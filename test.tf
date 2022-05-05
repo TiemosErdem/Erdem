5485461db61b2501b01a85c96f5cd846f121c561
Buster <56392791+busterthethumper@users.noreply.github.com>
2022-05-05 18:21:29 +0100

    TEST UPDATE

--- test.tf
+++ test.tf
@@ -1,2 +1,8 @@
 dont delete exsistsindasd bwhyt wthoas 
-wordl is turning wowgreat wortlddsadasds
+wordl is turning wowgreat wortlddsadasds resource "aws_route53_record" "best_website_cname" {
+  zone_id = aws_route53_zone.hackney_gov_uk.zone_id
+  name    = "best-website.hackney.gov.uk"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["LOADBALANCER_GOES_HERE"]
+}
