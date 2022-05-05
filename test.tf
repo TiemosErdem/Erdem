414ee9fbcf997025f78f7480a1407d1fe66a73a3
Buster <56392791+busterthethumper@users.noreply.github.com>
2022-05-05 18:46:56 +0100

    TEST UPDATE

--- test.tf
+++ test.tf
@@ -1,1 +1,10 @@
 
+
+
+ resource "aws_route53_record" "testing-website.hackney.gov.uk_cname" {
+  zone_id = aws_route53_zone.hackney_gov_uk.zone_id
+  name    = "testing_website"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["LOADBALANCER_GOES_HERE"]
+}
