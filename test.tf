c5a627a6b4abdbce68381b68fcdfb4b0a3805995
Buster <56392791+busterthethumper@users.noreply.github.com>
2022-05-06 13:10:49 +0100

    TEST UPDATE

--- test.tf
+++ test.tf
@@ -55,3 +55,10 @@ By busterthethumper [TEST TERRAFORM FILE - INFORMATION IN THIS FILE IS UPDATED M
   ttl     = "3600"
   records = ["LOADBALANCER_GOES_HERE"]
 }
+ resource "aws_route53_record" "frank_is_awesome_com_cname" {
+  zone_id = ZONE_ID_GOES_HERE
+  name    = "frankisawesome.com"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["LOADBALANCER_GOES_HERE"]
+}
